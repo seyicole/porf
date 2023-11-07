fe9965407a786450737c867bc379935fc870e524
Oluseyi Imanah <seyicole@gmail.com>
2023-11-07 20:26:55 +0000

    change colors and drawing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { links } from '../data';
 
 const Navbar = () => {
   return (
-    <nav className='bg-emerald-100'>
+    <nav className='bg-blue-100'>
       <div className='align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
         <h2 className='text-3xl font-bold'>
-          Olu<span className='text-emerald-600'>Dev</span>
+          Olu<span className='text-blue-600'>Dev</span>
         </h2>
         <div className='flex gap-x-3'>
           {links.map((link) => {
@@ -14,7 +14,7 @@ const Navbar = () => {
               <a
                 key={id}
                 href={href}
-                className='capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'
+                className='capitalize text-lg tracking-wide hover:text-blue-600 duration-300'
               >
                 {text}
               </a>
